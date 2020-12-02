362435b34444ea46ef47621147492b5232177909
Edu <educharrone@hotmail.com>
2020-12-02 18:52:48 -0300

    H2 database, test , profile , JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,18 +2,27 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
-	public User() {}
-	
+
+	public User() {
+	}
+
 	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
@@ -87,6 +96,4 @@ public class User implements Serializable {
 		return true;
 	}
 
-	
-	
 }
